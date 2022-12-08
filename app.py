7583584f30f10a6d691560c2079f546dea2cbff3
Александр Матияка <matiacka@yandex.ru>
2022-12-08 14:17:22 +0300

    CRUD FLASK APP

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def create():
         content = request.form['content']
 
         if not title:
-            flash('Title is required!')
+            flash('Заполните название!')
         elif not content:
-            flash('Content is required!')
+            flash('Заполните текст!')
         else:
             conn = get_db_connection()
             conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
@@ -41,6 +41,39 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+
+@app.route('/posts/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route('/posts/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" был успешно удален!'.format(post['title']))
+    return redirect(url_for('index'))
 
 
 def get_db_connection():
